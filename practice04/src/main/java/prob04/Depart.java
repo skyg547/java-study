120c8117461605d9274f6c3416585dd3a4a99274
skyg547 <skyg547@gmail.com>
2020-07-25 23:19:03 +0900

    연습문제 4

--- practice04/src/main/java/prob04/Depart.java
+++ practice04/src/main/java/prob04/Depart.java
@@ -1,5 +1,19 @@
 package prob04;
 
 public class Depart extends Employee {
+	private String department;
+	
+	public Depart(String string, int i, String string2) {
+		super(string, i);
+		department = string2;
+	}
 
+	
+	public void getInformation() {
+		// TODO Auto-generated method stub
+		super.getInformation();
+		System.out.println("부서:"+department);
+	}
+	
+	
 }
