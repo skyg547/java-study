d1767d6160b148093bbbf92b658d802b2f514c1b
skyg547 <skyg547@gmail.com>
2021-01-23 02:46:51 +0900

    maven 없이 빌드

---
+++ chapter02/src/main/java/kr/ac/gachon/Main.java
@@ -0,0 +1,11 @@
+import  org.slf4j.Logger;
+import org.slf4j.LoggerFactory
+
+class Main{
+	Logger logger = LoggerFactory.getLogger(Main.class)
+
+	public static void main(String[] args){
+		System.out.println("Hello world!!");
+		logger.info("hello world!!")
+	}
+}
